eadce323800b9b2db555a00a5f237cfd28c69bd8
lyc425 <1104574254@qq.com>
2022-04-18 15:47:46 +0800

    third add by lyc

---
+++ online_edu/src/main/java/com/ssm/service/impl/UserServiceImpl.java
@@ -0,0 +1,119 @@
+package com.ssm.service.impl;
+
+import com.ssm.dao.UserDao;
+import com.ssm.entity.User;
+import com.ssm.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * (User表)服务实现类
+ *
+ * @author makejava
+ * @since 2022-04-17 23:34:45
+ */
+@Service("userService")
+public class UserServiceImpl implements UserService {
+    @Resource
+    private UserDao userDao;
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    @Override
+    public User selectById(Integer id) {
+        return this.userDao.selectById(id);
+    }
+
+    /**
+     * 分页查询
+     *
+     * @param start 查询起始位置
+     * @param limit 查询条数
+     * @return 对象列表
+     */
+    @Override
+    public List<User> selectPage(int start, int limit) {
+        return this.userDao.selectPage(start, limit);
+    }
+
+    /**
+     * 查询所有
+     *
+     * @return 实例对象的集合
+     */
+     @Override
+     public List<User> selectAll() {
+        return this.userDao.selectAll();
+     }
+     
+    /**
+     * 根据条件查询
+     *
+     * @return 实例对象的集合
+     */
+    @Override
+    public List<User> selectList(User user) {
+        return this.userDao.selectList(user);
+    }
+    
+    /**
+     * 新增数据
+     *
+     * @param user 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public int insert(User user) {
+        return this.userDao.insert(user);
+    }
+
+    /**
+     * 批量新增
+     *
+     * @param users 实例对象的集合
+     * @return 生效的条数
+     */
+    @Override
+    public int batchInsert(List<User> users) {
+        return this.userDao.batchInsert(users);
+    }
+
+    /**
+     * 修改数据
+     *
+     * @param user 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public User update(User user) {
+        this.userDao.update(user);
+        return this.selectById((Integer) user.getId());
+    }
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    @Override
+    public int deleteById(Integer id) {
+        return this.userDao.deleteById(id);
+    }
+    
+    /**
+     * 查询总数据数
+     *
+     * @return 数据总数
+     */
+     @Override
+     public int count(){
+        return this.userDao.count();
+     }
+}
